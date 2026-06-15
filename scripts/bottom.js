9d214f93a2cfad1623e6942f672f193052d3c041
Lucas Cutecat <shenqian@apps.singapore.edu.hk>
2026-06-15 20:20:17 +0800

    i had a weird feeling about this
    This reverts commit bf63d43eec8bae6491c0e9a869fca8fb0379db85, reversing
    changes made to 4877cb0e82a274c2a6a0e8fe7f57a7e34dde19c2.

--- scripts/bottom.js
+++ scripts/bottom.js
@@ -3,7 +3,7 @@ var img = document.getElementById("myImg");
 var modalImg = document.getElementById("img01");
 img.onclick = function(){
   	modal.style.display = "block";
-   	modalImg.src = "resources/bottomlicense.avif";
+  	modalImg.src = "resources/bottomlicense.png";
 }
 var span = document.getElementsByClassName("close")[0];
 span.onclick = function() { 
