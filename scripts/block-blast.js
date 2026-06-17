5dcec51465797c1b5ba0c2beece895a77dad3a8d
Calamariclam <76179328+Calamariclam@users.noreply.github.com>
2026-06-16 22:57:29 -0700

    Update block-blast.js

--- scripts/block-blast.js
+++ scripts/block-blast.js
@@ -1244,7 +1244,7 @@
                     if (ball.type === 'pink') {
                         // Pink hits paddle: make pink bigger
                         //if diamond paddle is bigger than pink, make size of diamond, then add half of the extra, otherwise just add half of diamond paddle
-                        pinkPaddle.h = (pinkPaddle.h <= diamondPaddle.h) ? diamodnPaddle.h + pinkPaddle.h / 2 : pinkPaddle.h + diamondPaddle.h/2
+                        pinkPaddle.h = (pinkPaddle.h <= diamondPaddle.h) ? diamondPaddle.h + pinkPaddle.h / 2 : pinkPaddle.h + diamondPaddle.h/2
                         // pinkPaddle.h += diamondPaddle.h
                         balls.splice(i, 1)
                         score(5, COLORS.PINK)
